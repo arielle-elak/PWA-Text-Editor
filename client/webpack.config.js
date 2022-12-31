9f7601970885777ade5488481707618cacebe339
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-31 15:26:45 -0700

    Added css loader that will use babel for the compiling

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,54 +10,73 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // All static assets (HTML, CSS, JS) will have the bundle.js at the end of the file once bundled for offline
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: "[name].bundle.js",
       // Path will be the current directory, then the dist sub folder
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       // HTML webpack plugin
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Text Editor',
+        template: "./index.html",
+        title: "Text Editor",
       }),
 
       // src-sw.js manifest Injector
       new InjectManifest({
-        swSrc: './src-sw.js',
+        swSrc: "./src-sw.js",
         // Put in dist folder
-        swDest: 'src-sw.js',
+        swDest: "src-sw.js",
       }),
 
       // Creates a manifest.json file with the following attributes for styling the app
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Text Editor',
-        short_name: 'Editor',
-        description: 'Write and edit text easily.',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        name: "Text Editor",
+        short_name: "Editor",
+        description: "Write and edit text easily.",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
     ],
 
     module: {
+      // CSS loaders using the babel compiler
       rules: [
-
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
