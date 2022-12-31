daa9cbde61a70ded31fc6a7384a5b2e0940f3d57
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-31 15:36:42 -0700

    Going through TODOS, database.js settting up proper formula format

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,9 @@
 import { openDB } from 'idb';
 
+// Database is called 'jate' using version 1
 const initdb = async () =>
   openDB('jate', 1, {
+    // If db schema has not been already added, add it!
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -13,9 +15,13 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+}
 
 initdb();
