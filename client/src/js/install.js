dddb6959ddfc0e9975bfa32e07c50e859f1fa462
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-31 16:40:04 -0700

    Trying some troubleshooting steps - installed jquery just in case

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,18 +1,18 @@
 // Install script for installing the PWA to local computer
-const butInstall = document.getElementById("buttonInstall");
+const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 
 // Add an event handler to the `beforeinstallprompt` event
-window.addEventListener("beforeinstallprompt", (event) => {
+window.addEventListener('beforeinstallprompt', (event) => {
   // Store the triggered events
   window.deferredPrompt = event;
   // Remove the hidden class from the button.
-  butInstall.classList.toggle("hidden", false);
+  butInstall.classList.toggle('hidden', false);
 });
 
 // Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {
+butInstall.addEventListener('click', async () => {
   // Call the stored event
   const promptEvent = window.deferredPrompt;
 
@@ -27,11 +27,11 @@ butInstall.addEventListener("click", async () => {
   window.deferredPrompt = null;
 
   // Re-hide the button
-  butInstall.classList.toggle("hidden", true);
+  butInstall.classList.toggle('hidden', true);
 });
 
 // Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {
+window.addEventListener('appinstalled', (event) => {
   // Double check that the prompt has indeed been cleared from the previous function
   window.deferredPrompt = null;
 });
