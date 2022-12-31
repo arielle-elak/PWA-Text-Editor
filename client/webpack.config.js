9f6e5a3e5e863b7a04d8d52c09e900e18c950301
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-31 15:21:29 -0700

    Implementing plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,5 @@
+// General Webpack compiler config
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
@@ -6,6 +8,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
+// All static assets (HTML, CSS, JS) will have the bundle.js at the end of the file once bundled for offline
 module.exports = () => {
   return {
     mode: 'development',
@@ -15,15 +18,26 @@ module.exports = () => {
     },
     output: {
       filename: '[name].bundle.js',
+      // Path will be the current directory, then the dist sub folder
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // HTML webpack plugin
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor',
+      }),
+
+      // src-sw.js Injector
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+
       ],
     },
   };
